546e9339357c12aabdadf8c7cff6cf041369ec15
Benhida-Oussama <benhidaoussama@gmail.com>
2025-12-15 17:56:39 +0100

    chore(mobile): Update app config and entry point

--- CoLearn/lib/main.dart
+++ CoLearn/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:colearn/views/splash/splash.dart';
+import 'package:colearn/views/auth/login.dart'; // Import Login
+import 'package:colearn/views/auth/signup.dart'; // Import Signup
+import 'package:colearn/views/home/home.dart';   // Import Home
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'consts/consts.dart';
@@ -16,10 +19,19 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: appname,
       theme: ThemeData(
-          scaffoldBackgroundColor: Colors.transparent,
-          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
-          fontFamily: regular),
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+        fontFamily: regular,
+      ),
+      // Start with the Splash Screen
       home: const SplashScreen(),
+
+      // Define the Named Routes here
+      getPages: [
+        GetPage(name: '/LoginScreen', page: () => const LoginScreen()),
+        GetPage(name: '/SignupScreen', page: () => const SignupScreen()),
+        GetPage(name: '/HomeScreen', page: () => const HomeScreen()),
+      ],
     );
   }
 }
